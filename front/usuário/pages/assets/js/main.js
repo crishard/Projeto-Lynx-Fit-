e5f00481a400cbf9736e3576ce3fcc43252711ea
crishard <crislantorres9@gmail.com>
2022-01-04 14:35:49 -0300

    login e home page-cliente 90%

---
+++ front/usuário/pages/assets/js/main.js
@@ -0,0 +1,12 @@
+// home page
+// Configuração de Menu
+const btnMobile = document.getElementById('btn-mobile');
+
+function botaoMenu(event){
+    if(event.type == 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', botaoMenu);
+btnMobile.addEventListener('touchstart', botaoMenu)
