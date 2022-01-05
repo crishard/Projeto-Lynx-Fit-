93cbec181e80cde7b5717c9d42a42d185304d8d0
crishard <crislantorres9@gmail.com>
2022-01-04 23:53:34 -0300

    login e home page cliente completo

--- front/cliente/pages/assets/js/home-page.js
+++ front/cliente/pages/assets/js/home-page.js
@@ -7,27 +7,3 @@ function botaoMenu(event){
 }
 btnMobile.addEventListener('click', botaoMenu);
 btnMobile.addEventListener('touchstart', botaoMenu);
-//função para coletar o valor clicado e mostrá-lo na tela
-function insert(num){ 
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
-}
-//função para limpar a calculadora
-function clean(){
-    document.getElementById('resultado').innerHTML = " ";
-}
-//função para apagar último dígito
-function back(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-}
-//função que vai calcular todos os valores
-function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;   
-    //verifica se existe algum valor escolhido e realiza o cálculo
-    if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado);
-    }else{
-        document.getElementById('resultado').innerHTML = "Nada..."
-    }
-}
