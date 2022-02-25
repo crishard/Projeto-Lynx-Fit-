a773866ab68f3a4331a1fa50b1d75065bf517190
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2022-02-25 16:05:15 -0300

    alteração equipamentos

--- back/src/modules/Treino/BuscarTreino/BuscarTreinoController.ts
+++ back/src/modules/Treino/BuscarTreino/BuscarTreinoController.ts
@@ -3,9 +3,10 @@ import { BuscarTreino } from "./BuscarTreino";
 
 export class BuscarTreinoController {
   async handle(req: Request, res: Response) {
+    const { id_client } = req;
     const buscarTreino = new BuscarTreino();
 
-    const result = await buscarTreino.execute();
+    const result = await buscarTreino.execute(id_client);
 
     return res.json(result);
   }
