9b171c9fe9120bd241f9d48999154730d0ef21fb
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2022-02-25 16:08:21 -0300

    alteração get cliente

--- back/src/modules/Cliente/BuscarCliente/BuscarCliente.ts
+++ back/src/modules/Cliente/BuscarCliente/BuscarCliente.ts
@@ -1,15 +1,15 @@
 import { prisma} from "../../../dataBase/prismaCliente";
 
 interface IBuscarCliente{
-    id: string;
+  id_client: string;
 }
 export class BuscarCliente{
-    async execute({id}: IBuscarCliente){
-        const buscarCliente = await prisma.cliente.findUnique({
-            where: {
-              id: id
-            },
-          })
-        return buscarCliente;
-    }
+  async execute({id_client}: IBuscarCliente){
+      const buscarCliente = await prisma.cliente.findUnique({
+          where: {
+            id: id_client
+          },
+        })
+      return buscarCliente;
+  }
 }
