1f9cab2f086ac6622c5066eb97db8f17a98821d6
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2022-02-24 21:26:44 -0300

    ateração buscar usuario

--- back/src/modules/Usuario/BuscarUsuario/BuscarUsuarioController.ts
+++ back/src/modules/Usuario/BuscarUsuario/BuscarUsuarioController.ts
@@ -3,9 +3,10 @@ import { BuscarUsuario } from "./BuscarUsuario";
 
 export class BuscarUsuarioController {
   async handle(req: Request, res: Response) {
+    const { id_client } = req;
     const buscarUsuario = new BuscarUsuario();
 
-    const resultado = await buscarUsuario.execute();
+    const resultado = await buscarUsuario.execute(id_client);
 
     return res.json(resultado);
   }
