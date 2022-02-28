b93b7b403249a36731da4d53aaa78b51de216176
crishard <crislantorres9@gmail.com>
2022-02-28 17:06:08 -0300

    exibir rotina

--- Front/requisicoes/usuario/rotinaUsuario.js
+++ Front/requisicoes/usuario/rotinaUsuario.js
@@ -1,7 +1,6 @@
 // capturando o token de autenticação
 const pegaToken = localStorage.getItem('token');
 const token = pegaToken.replace(/^"(.+(?="$))"$/, '$1');
-
 const config = {
     headers: { Authorization: `Bearer ${token}` }
 }
@@ -14,20 +13,14 @@ function getUsuario() {
         const data = response.data
         mostrarUsuarios(data)
     })
-
 }
 getUsuario();
 
-
-
 function mostrarUsuarios(users2){
     let usuario = "";
-    usuario += `<option value="${users2.nome}" id="nomeUser">Mostrar Rotina</option> 
-    <option value="esconder" id="esconder">Esconder Rotina</option>`
+    usuario += `<option value="${users2.nome}" id="nomeUser">Mostrar Rotina</option>`
     document.getElementById("usuarioOp").innerHTML = usuario;
 }
-
-
 
 const vizualizar = document.getElementById('comfirmar');
 const usuarioEscolhido = document.getElementById('usuarioOp')
@@ -36,58 +29,23 @@ vizualizar.addEventListener('click', ()=>{
     const elemento = usuarioEscolhido.value;
     const nome_usuario = elemento
     
-    if(elemento != "esconder"){
-        console.log(nome_usuario);
-         
-        axios.get("http://localhost:3000/treino_usuario/"+`${nome_usuario}`)
-        .then(response => {
-            const data = response.data;
-            console.log(data);
-            // alert(JSON.stringify("Usuário apagado, recarregue a página"))
-        })
-        .catch(error => console.error(error));
-    }
-
-    if(elemento == "esconder"){
-        document.getElementById("mostrar").classList.add("tabela-desativada")
-    }
-
+    axios.get("http://localhost:3000/treino_usuario/"+`${nome_usuario}`)
+    .then(response => {
+        const data = response.data;
+        show(data);
+    })
+    .catch(error => console.error(error));
 })
-
-
-
-
-
-
-
-// function opitionEscondida(nome){
-//     let usuario = "";
-//         usuario += `<a value="${nome.nome}" id="salvou"></a>`;
-//     document.getElementById("some").innerHTML = usuario;
-//     // console.log(usuario)
-// }
-
-
-
-// const vizualizar = document.getElementById("salvou");
-// const bip = vizualizar.value;
-// // const nomeMatheux = document.getElementById("salvou");
-// // const nome = nomeMatheux.value;
-// console.log(bip);
-
-
-
-// function show(users) {
-//     for (use of users){
-//         const newRow = document.createElement('tr')
-//             newRow.innerHTML = `
-//                 <td>${use.treino}</td>
-//                 <td>${use.series}</td>
-//                 <td>${use.repeticoes}</td>
-//                 <td>${use.nome_equipamento}</td>
-//                 `
-//                 document.getElementById('mostrar').appendChild(newRow);
-//     }
-
-// }
-
+//imprimir os usuario na tela
+function show(users) {
+    for (use of users){
+        const newRow = document.createElement('tr')
+            newRow.innerHTML = `
+                <td>${use.treino}</td>
+                <td>${use.series}</td>
+                <td>${use.repeticoes}</td>
+                <td>${use.nome_equipamento}</td>
+                `
+                document.getElementById('mostrar').appendChild(newRow);
+    }
+}
